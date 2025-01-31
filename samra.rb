591996de400e6bc4e3766b39837474ec83060e7e
unknown <regime_skylark.0y@icloud.com>
2025-01-30 20:31:03 -0500

    Simplifed the formula

--- samra.rb
+++ samra.rb
@@ -1,17 +1,12 @@
 cask "samra" do
-  version "v1.4.1"
+  version :latest
   sha256 :no_check
   url "https://github.com/NSAntoine/Samra/releases/latest/download/Samra.app.zip"
   name "Samra"
   desc "Native Asset Catalog explorer & editor"
   homepage "https://github.com/NSAntoine/Samra"
 
-  livecheck do
-    url :url
-    strategy :github_latest
-  end
-
-  auto_updates true
+  auto_updates false
   depends_on macos: ">= :catalina"
 
   app "Samra.app"
