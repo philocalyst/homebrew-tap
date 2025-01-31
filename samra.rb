74d7d80b01f74f0ff12080ad646584888e99fbd4
unknown <regime_skylark.0y@icloud.com>
2025-01-30 20:20:03 -0500

    Cask formulae for Samra
    Didn't get merged because is not notarized. At your own risk!

---
+++ samra.rb
@@ -0,0 +1,41 @@
+cask "samra" do
+  version "1.4.1"
+  license "MIT"
+  sha256 :no_check
+  url "https://github.com/NSAntoine/Samra/releases/latest/download/Samra.app.zip"
+  name "Samra"
+  desc "Native Asset Catalog explorer & editor"
+  homepage "https://github.com/NSAntoine/Samra"
+
+  livecheck do
+    url :url
+    strategy :github_latest
+  end
+
+  auto_updates true
+  depends_on macos: ">= :catalina"
+
+  app "Samra.app"
+
+  postflight do
+    system_command "/usr/bin/xattr",
+                   args:         ["-rd", "com.apple.quarantine", "#{appdir}/Samra.app"],
+                   sudo:         false,
+                   print_stderr: false
+  end
+
+  zap trash: [
+    "~/Library/Application Support/com.NSAntoine.Samra",
+    "~/Library/Caches/com.NSAntoine.Samra",
+    "~/Library/Preferences/com.NSAntoine.Samra.plist",
+    "~/Library/Saved Application State/com.NSAntoine.Samra.savedState",
+  ]
+
+  caveats do
+    <<~EOS
+      Samra requires Xcode Command Line Tools to be installed.
+      You can install them with:
+        xcode-select --install
+    EOS
+  end
+end
