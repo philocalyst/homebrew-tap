eff6aa93df7eb181406e89e7c62272387cf6efdf
unknown <regime_skylark.0y@icloud.com>
2025-04-26 20:08:49 -0400

    💫CHANGED💫 v2.2.0

--- infat.rb
+++ infat.rb
@@ -1,7 +1,7 @@
 class Infat < Formula
   desc "Set default openers for file formats and url schemes"
   homepage "https://github.com/philocalyst/infat"
-  version "2.0.0"
+  version "2.2.0"
   license "MIT"
   head "https://github.com/philocalyst/infat.git", branch: "main"
 
@@ -15,10 +15,10 @@ class Infat < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/#{author}/infat/releases/download/v#{version}/infat-arm64-apple-macos.tar.gz"
-      sha256 "fb061f523ac1742be3461c629b7b442c7c2bc29ce37437c641a3c8c2e0ecfaf0"
+      sha256 "3475be07889be9a46f3a7a4a28111e4bd4954d6982bc751894e1e9616d0d8e82"
     else
       url "https://github.com/#{author}/infat/releases/download/v#{version}/infat-x86_64-apple-macos.tar.gz"
-      sha256 "110520811c953dd4f7b1d66fc8145f6a0c710635598fa8ca5bc2074db603d603"
+      sha256 "6f9eabb821e65082867bf94f0a6158ad5194bd9d55e06a669fa997cca9e57bbf"
     end
   end
 
