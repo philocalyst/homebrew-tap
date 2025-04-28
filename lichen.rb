c1df43fc22ec4f2886b8477b0ebb26c1cbfdbb23
unknown <regime_skylark.0y@icloud.com>
2025-04-28 13:18:25 -0400

    💥ADDED💥 Lichen package

---
+++ lichen.rb
@@ -0,0 +1,38 @@
+class Lichen < Formula
+  desc "Command-line tool to manage license files and headers in software projects"
+  homepage "https://github.com/philocalyst/lichen"
+  version "0.3.5"
+  license "MIT"
+  head "https://github.com/philocalyst/lichen.git", branch: "main"
+
+  livecheck do
+    url :stable
+    strategy :github_latest
+  end
+
+  author = "philocalyst"
+
+  on_macos do
+    if Hardware::CPU.arm?
+      url "https://github.com/#{author}/lichen/releases/download/v#{version}/lic-aarch64-apple-darwin.tar.gz"
+    else
+      url "https://github.com/#{author}/lichen/releases/download/v#{version}/lic-x86_64-apple-darwin.tar.gz"
+    end
+  end
+
+  on_linux do
+    if Hardware::CPU.arm? && Hardware::CPU.is_64_bit?
+      url "https://github.com/#{author}/lichen/releases/download/v#{version}/lic-aarch64-unknown-linux-musl.tar.gz"
+    elsif Hardware::CPU.intel?
+      url "https://github.com/#{author}/lichen/releases/download/v#{version}/lic-x86_64-unknown-linux-musl.tar.gz"
+    end
+  end
+
+  def install
+    bin.install("lic")
+  end
+
+  test do
+    system bin / "lic", "--version"
+  end
+end
