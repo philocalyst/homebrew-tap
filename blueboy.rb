9de8f3ea3bbace0fa0300369344f9862e430559c
unknown <regime_skylark.0y@icloud.com>
2025-05-09 15:38:47 -0400

    💫CHANGED💫 Bump!!

--- blueboy.rb
+++ blueboy.rb
@@ -1,7 +1,7 @@
 class BlueBoy < Formula
   desc "Set default openers for file formats and url schemes"
   homepage "https://github.com/philocalyst/blueboy"
-  version "1.1.1"
+  version "1.1.2"
   license "MIT"
   head "https://github.com/philocalyst/blueboy.git", branch: "main"
 
@@ -15,10 +15,8 @@ class BlueBoy < Formula
   on_macos do
     if Hardware::CPU.arm?
       url "https://github.com/#{author}/blueboy/releases/download/v#{version}/blueboy-arm64-apple-macos.tar.gz"
-      sha256 "6b45f011f5c5f8e1f7c9d77d947a028663882f97f8892825c3aff45fe76b8876"
     else
       url "https://github.com/#{author}/blueboy/releases/download/v#{version}/blueboy-x86_64-apple-macos.tar.gz"
-      sha256 "d9b8d192b8f6eadedc4ad00ab7828612d08e4780006e4b62b4db5a973807f5ce"
     end
   end
 
